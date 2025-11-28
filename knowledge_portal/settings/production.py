aa5cbb782340bd57ba17b4d708e88ea61ae40c03
jchemutt <chemuttjose@gmail.com>
2025-11-28 09:48:41 +0300

    first commit

--- knowledge_portal/settings/production.py
+++ knowledge_portal/settings/production.py
@@ -1,6 +1,8 @@
 from .base import *
 
-DEBUG = False
+DEBUG = bool(data.get("debug", False))
+SECRET_KEY = data.get("secret_key", "django-insecure-xyzmc*)97o3=r81+s$9*1y2!he73!*_clk$1w-0vg6wl2c-re7")  # fall back to base if missing
+ALLOWED_HOSTS = data.get("allowed_hosts", [])
 
 # ManifestStaticFilesStorage is recommended in production, to prevent
 # outdated JavaScript / CSS assets being served from cache
