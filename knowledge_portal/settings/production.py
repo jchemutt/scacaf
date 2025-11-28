33438a8df96c061322d009b5e984ac53e9d68f12
jchemutt <chemuttjose@gmail.com>
2025-11-28 10:28:46 +0300

    first commit

--- knowledge_portal/settings/production.py
+++ knowledge_portal/settings/production.py
@@ -14,3 +14,28 @@ try:
     from .local import *
 except ImportError:
     pass
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "django_errors.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        "wagtail": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+
