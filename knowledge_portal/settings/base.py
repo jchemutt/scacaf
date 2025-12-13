0a77b763ad215f00316c7eb12d0318379ae49766
jchemutt <chemuttjose@gmail.com>
2025-12-13 22:02:53 +0300

    pdf fix

--- knowledge_portal/settings/base.py
+++ knowledge_portal/settings/base.py
@@ -192,3 +192,5 @@ WAGTAILADMIN_BASE_URL = "http://example.com"
 # if untrusted users are allowed to upload files -
 # see https://docs.wagtail.org/en/stable/advanced_topics/deploying.html#user-uploaded-files
 WAGTAILDOCS_EXTENSIONS = ['csv', 'docx', 'key', 'odt', 'pdf', 'pptx', 'rtf', 'txt', 'xlsx', 'zip']
+
+X_FRAME_OPTIONS = "SAMEORIGIN"
